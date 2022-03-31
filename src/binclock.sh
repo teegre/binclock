9bd0700f74c596523313149afa82392d1f60cf19
Stéphane MEYER <tigerlost@me.com>
2022-03-31 15:22:36 +0200

    refactored + display date

--- src/binclock.sh
+++ src/binclock.sh
@@ -24,7 +24,7 @@
 #
 # BinClock
 # C : 2022/03/29
-# M : 2022/03/30
+# M : 2022/03/31
 # D : A binary clock.
 
 _help() {
@@ -41,6 +41,7 @@ BinClock ver 0.1
 
   Key Bindings:
     c: change color.
+    d: display date.
     q: quit.
     r: redraw the clock.
 EOH
@@ -53,23 +54,23 @@ EOH
 
 [[ $1 == "-m" ]] && { f=1; shift; }
 
-[[ $1 ]] && {
+(( $# > 0 )) && {
   >&2 echo "Error: invalid option: $*"
   exit 1
 }
 
-B=("0000" "0001" "0010" "0011" "0100" "0101" "0110" "0111" "1000" "1001")
+BIN=("0000" "0001" "0010" "0011" "0100" "0101" "0110" "0111" "1000" "1001")
 
-b="\e[1m"      # bold
-d="\e[2m"      # dim
-c="\e[38;5;4m" # color
-r="\e[m"       # reset
+bold="\e[1m"
+dim="\e[2m"
+# color="\e[38;5;0m"
+rst="\e[m" # reset
 
 hidecursor()    { echo -ne "\e[?25l"; }
 showcursor()    { echo -ne "\e[?25h"; }
 locate()        { local y x; y=$1; x=$2; printf '\e[%d;%dH' $((y)) $((x)); }
 get_scr_size()  { shopt -s checkwinsize; (:;:); }
-random_color()  { local C; ((C=(RANDOM%254)+1)); c="\e[38;5;${C}m"; }
+random_color()  { local C; ((C=(RANDOM%253)+2)); color="\e[38;5;${C}m"; }
 
 exec {pause_fd}<> <(:)
 pause() ( read -rt "$1" -u $pause_fd )
@@ -84,124 +85,143 @@ _sync() {
   done
 }
 
-hidecursor; stty -echo -icanon time 0 min 0; clear
+hidecursor; stty -echo -icanon time 0 min 0
 
 trap 'echo -en "\e[m"; showcursor; stty sane; exit' INT QUIT
-trap 'init_screen; h=""; m=""; s=""; p=""; clear' WINCH
+trap 'init_screen; reset_var' WINCH
 
 init_screen() {
+  clear;
   get_scr_size
-  ((oy=(LINES/2)-1))
-  ((ox=(COLUMNS/2)-3))
+  ((OY=(LINES/2)-1))
+  ((OX=(COLUMNS/2)-3))
 }
 
-init_screen
-random_color
+reset_var() { unset hy mm sd ind; }
 
-declare h m s p
+display_bin_digit() {
+  local y x D g i
+
+  y=$1; x=$2; D=$3
+  g=8
+
+  for ((i=0;i<${#D};i++)); do
+
+    locate $((y)) $((x))
+
+    (( ${D:i:1} == 1 )) && {
+      echo -en "${color}${bold}1${rst}"
+      locate $((y-1)) $((x))
+      echo -en "${color}${bold}${g}${rst}"
+    }
+
+    (( ${D:i:1} == 0 )) && {
+      echo -en "${color}${dim}0${rst}"
+      locate $((y-1)) $((x))
+      echo -en "${color}${dim}.${rst}"
+    }
+
+    ((x++))
+    ((g=g == 1 ? 8 : g/2))
+
+  done
+}
+
+display() {
+  local DT A B C y x
+
+  [[ $SHOWDATE ]] || {
+    [[ $f ]] && DT="$(date '+%H %M %S')"
+    [[ $f ]] || DT="$(LC_TIME=C date '+%I%p %M %S ')"
+  }
+
+  [[ $SHOWDATE ]] && {
+    DT="$(date '+%y %m %d')"
+  }
+  
+  # shellcheck disable=SC2162
+  IFS=' ' read A B C <<< "$DT"
+
+  AHY="${BIN[${A:0:1}]}${BIN[${A:1:1}]}"
+  AMM="${BIN[${B:0:1}]}${BIN[${B:1:1}]}"
+  ASD="${BIN[${C:0:1}]}${BIN[${C:1:1}]}"
+
+  [[ $SHOWDATE ]] || { [[ $f ]] || { [[ ${A:2:1} == "P" ]] && AIND=1 || AIND=0; } ;}
+
+  ((y=OY))
+
+  [[ $AHY != "$hy" ]] && { # ensures we only print when it's needed.
+    hy=$AHY
+    display_bin_digit $((y)) $((OX)) "$hy"
+  }
+
+  ((y+=2))
+
+  [[ $AMM != "$mm" ]] && {
+    mm=$AMM
+    display_bin_digit $((y)) $((OX)) "$mm"
+  }
+
+  ((y+=2))
+
+  [[ $ASD != "$sd" ]] && {
+    sd=$ASD
+    display_bin_digit $((y)) $((OX)) "$sd"
+  }
+
+  [[ $SHOWDATE ]] || { [[ $f ]] || { [[ $ind != "$AIND" ]] && {
+    ind=$AIND
+    locate $((y)) $((OX+8))
+    ((ind == 1)) && echo -en "${color}${dim}1${rst}"
+    ((ind == 0)) && echo -en "${color}${dim}0${rst}"
+    locate $((y-1)) $((OX+8))
+    ((ind == 1)) && echo -en "${color}${dim}P${rst}"
+    ((ind == 0)) && echo -en "${color}${dim}A${rst}"
+  } } }
+
+  [[ $SHOWDATE ]] && { [[ $ind != "$AIND" ]] && {
+    ind=$AIND
+    locate $((y)) $((OX+8))
+    echo -en "${color}${dim}.${rst}"
+    locate $((y-1)) $((OX+8))
+    echo -en "${color}${dim}D${rst}"
+  } }
+}
+
+# AHY  → actual hours/year
+# AMM  → actual minutes/month
+# ASD  → actual seconds/day
+# AIND → actual indicator (A P or D)
+# hy   → displayed hours/year
+# mm   → displayed minutes/month
+# sd   → displayed seconds/day
+# ind  → displayed indicator (A P or D)
+#
+# OY, OX → clock top left position
+declare AHY AMM ASD AIND hy mm sd ind OY OX
+
+init_screen
 
 while :; do
   # shellcheck disable=SC2162
   IFS= read key
   case $key in
     q | Q) clear; break ;;
-    r | R) clear; init_screen; h=""; m=""; s=""; p="" ;;
-    c | C) random_color; h=""; m=""; s=""; p="" ;;
+    r | R) init_screen; reset_var ;;
+    c | C) random_color; reset_var ;;
+    d | D) [[ $SHOWDATE ]] || { SHOWDATE=1; ((TIMER=EPOCHSECONDS)); reset_var; } ;;
   esac
 
   _sync
 
-  [[ $f ]] && TIME="$(date '+%H %M %S')"
-  [[ $f ]] || TIME="$(LC_TIME=C date '+%I%p %M %S ')"
-  
-  # shellcheck disable=SC2162
-  IFS=' ' read H M S <<< "$TIME"
-
-  oh="${B[${H:0:1}]}${B[${H:1:1}]}"
-  om="${B[${M:0:1}]}${B[${M:1:1}]}"
-  os="${B[${S:0:1}]}${B[${S:1:1}]}"
-  [[ $f ]] || { [[ ${H:2:1} == "P" ]] && op=1 || op=0; }
-
-  ((y=oy))
-  ((x=ox))
-  ((g=8))
-
-  [[ $oh != "$h" ]] && {
-    h=$oh
-    for ((i=0;i<${#h};i++)); do
-      locate $((y)) $((x))
-      (( ${h:i:1} == 1 )) && {
-        echo -en "${c}${b}1${r}"
-        locate $((y-1)) $((x))
-        echo -en "${c}${b}${g}${r}"
-      }
-      (( ${h:i:1} == 0 )) && {
-        echo -en "${c}${d}0${r}"
-        locate $((y-1)) $((x))
-        echo -en "${c}${d}.${r}"
-      }
-      ((x++))
-      ((g=g==1 ? 8 : g/2))
-    done
-  }
-
-  ((y+=2))
-  ((x=ox))
-
-  [[ $om != "$m" ]] && {
-    m=$om
-    for ((i=0;i<${#m};i++)); do
-      locate $((y)) $((x))
-      (( ${m:i:1} == 1 )) && {
-        echo -en "${c}${b}1${r}"
-        locate $((y-1)) $((x))
-        echo -en "${c}${b}${g}${r}"
-      }
-      (( ${m:i:1} == 0 )) && {
-        echo -en "${c}${d}0${r}"
-        locate $((y-1)) $((x))
-        echo -en "${c}${d}.${r}"
-      }
-      ((x++))
-      ((g=g==1 ? 8 : g/2))
-    done
-  }
-
-  ((y+=2))
-  ((x=ox))
-
-  [[ $os != "$s" ]] && {
-    s=$os
-    for ((i=0;i<${#s};i++)); do
-      locate $((y)) $((x))
-      (( ${s:i:1} == 1 )) && {
-        echo -en "${c}${b}1${r}"
-        locate $((y-1)) $((x))
-        echo -en "${c}${b}${g}${r}"
-      }
-      (( ${s:i:1} == 0 )) && {
-        echo -en "${c}${d}0${r}"
-        locate $((y-1)) $((x))
-        echo -en "${c}${d}.${r}"
-      }
-      ((x++))
-      ((g=g==1 ? 8 : g/2))
-    done
-  }
-
-  [[ $f ]] || { [[ $p != "$op" ]] && {
-    p=$op
-    locate $((y)) $((x))
-    ((p == 1)) && echo -en "${c}${d}1${r}"
-    ((p == 0)) && echo -en "${c}${d}0${r}"
-    locate $((y-1)) $((x))
-    ((p == 1)) && echo -en "${c}${d}P${r}"
-    ((p == 0)) && echo -en "${c}${d}A${r}"
-  } }
+  display
 
   pause 0.5
 
+  # date shows for 5 seconds
+  [[ $SHOWDATE ]] && ((EPOCHSECONDS - TIMER == 5 )) && { unset SHOWDATE TIMER; reset_var; };
+
 done
 
-echo -en "${r}"
+echo -en "${rst}"
 stty sane; showcursor
